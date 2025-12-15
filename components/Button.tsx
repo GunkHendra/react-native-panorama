3d1f77a8fee9800f8dccdbf4a49d220f46ffa2bb
Krishna <gunghencrypt@gmail.com>
2025-12-16 01:42:21 +0800

    refactor: change custombutton and customfloatingbutton way of checking icon or text exist
    refactor: adding text in customfloatingbutton

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,7 +3,7 @@ import { AntDesign } from '@expo/vector-icons';
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
 
-const CustomButton = ({ variant = "dark", classname, text, onPress, hasIcon, icon, isCenter, ...rest }: ButtonProps) => {
+const CustomButton = ({ variant = "dark", classname, text, onPress, icon = "", isCenter, ...rest }: ButtonProps) => {
 
     const variants: Record<ButtonVariant, string> = {
         dark: 'bg-primary',
@@ -13,10 +13,10 @@ const CustomButton = ({ variant = "dark", classname, text, onPress, hasIcon, ico
 
 
     return (
-        <Pressable className={`${variants[variant]} p-4 rounded-full h-14 ${classname} ${isCenter ? 'items-center' : ''}`} onPress={onPress}>
-            <View className='flex-1 flex-row justify-between'>
+        <Pressable className={`${variants[variant]} p-4 rounded-full h-14 flex-row  ${classname}`} onPress={onPress}>
+            <View className={`${isCenter ? 'items-center justify-center' : 'justify-between'} flex-row flex-1`}>
                 <Text className={`${variant === 'dark' ? "text-background" : (variant === 'light' ? "text-secondary" : "text-primary")} text-md`}>{text}</Text>
-                {hasIcon &&
+                {icon &&
                     <AntDesign
                         name={icon}
                         size={20}
@@ -28,36 +28,22 @@ const CustomButton = ({ variant = "dark", classname, text, onPress, hasIcon, ico
     )
 }
 
-const CustomFloatingButton = ({ onPress, classname, icon }: { onPress?: () => void, classname?: string, icon: any }) => {
+const CustomFloatingButton = ({ onPress, classname, icon, text = "" }: Partial<ButtonProps>) => {
     return (
-        <Pressable className={`bg-primary p-4 rounded-full h-14 w-14 justify-center items-center ${classname}`} onPress={onPress}>
+        <Pressable className={`bg-primary p-4 rounded-full ${text ? "flex-row gap-2" : "w-14"} h-14 justify-center items-center ${classname}`} onPress={onPress}>
+            {text &&
+                <Text className={`text-background text-md`}>{text}</Text>
+            }
             <AntDesign
                 name={icon}
-                size={22}
+                size={20}
                 color={'#FEFEFE'}
             />
         </Pressable>
     )
 }
 
-const CustomArrowButton = ({ onPress, classname, icon1, icon2, text }: { onPress?: () => void, classname?: string, icon1: any, icon2: any, text: string }) => {
-    return (
-        <Pressable className={`bg-primary p-3 rounded-full h-12 justify-center items-center gap-2 flex-row ${classname}`} onPress={onPress}>
-            <AntDesign
-                name={icon1}
-                size={18}
-                color={'#FEFEFE'}
-            />
-            <Text className={`text-background text-md ${classname}`}>{text}</Text>
-            <AntDesign
-                name={icon2}
-                size={18}
-                color={'#FEFEFE'}
-            />
-        </Pressable>
-    )
-}
 
 export default CustomButton;
-export { CustomArrowButton, CustomFloatingButton };
+export { CustomFloatingButton };
 
