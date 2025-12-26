d79b47906757dcef6c2bb4dab5f2f6e4e9884edd
Krishna <gunghencrypt@gmail.com>
2025-12-27 01:32:14 +0800

    chore: add color constant

---
+++ constants/color.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: "#010F1C",
+  secondary: "rgba(1, 15, 28, 0.40)",
+  border: "rgba(1, 15, 28, 0.12)",
+  background: "#FEFEFE",
+  neutral: {
+    100: "#F3F4F6",
+    200: "#E5E7EB",
+  },
+} as const;
